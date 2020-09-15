8b7babd53857587146ce9914a64866f79828a739
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2020-09-14 20:48:34 -0400

    se diseño la cabecera de la pagina de ajustes

--- lib/src/ui/setting/setting_screen.dart
+++ lib/src/ui/setting/setting_screen.dart
@@ -1,8 +1,12 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pymbo/src/bloc/authentication_bloc/bloc.dart';
 
 class SettingsScreen extends StatefulWidget {
+  final String photo;
+
+  const SettingsScreen({Key key, this.photo}) : super(key: key);
   @override
   _SettingsScreenState createState() => _SettingsScreenState();
 }
@@ -11,15 +15,36 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ajustes'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.exit_to_app), onPressed: (){
-            BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
-          })
-        ],
-      ),
-      body: Container(color: Colors.deepPurple),
-    );
+        backgroundColor: Color(0XFFF1FAEE),
+        body: Container(
+          width: double.infinity,
+          height: 200,
+          child: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                child: Image(
+                  image: AssetImage('assets/img/illimani.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 80,
+                  height: 80,
+                  child: CircularProfileAvatar(
+                    '',
+                    child: FlutterLogo(),
+                    radius: 80,
+                    borderColor: Color(0XFF1D3557),
+                    borderWidth: 3,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
